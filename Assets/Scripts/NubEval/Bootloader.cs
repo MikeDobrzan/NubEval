f526d38ae317b3ddc48112d2eb07ac883db8fd03
Radoslav Nikolaev <abix@abixinteractive.com>
2023-07-12 11:50:48 +0300

    Add Unity Project

---
+++ Assets/Scripts/NubEval/Bootloader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NubEval
+{
+    public class Bootloader : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Boot!");
+        }
+    }
+}
